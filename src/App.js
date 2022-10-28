1d00511e5063eba2d84f0c1e87eb629859988fb5
Maksymilian Malik <maksymilian.malik@gmail.com>
2022-10-28 18:32:33 +0200

    Add basics, background

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import "./reset.css"
+import MainPage from './Components/MainPage/MainPage';
+import NavBar from './Components/NavBar/NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <MainPage/>
+    </>
+
+  );
+}
+
+export default App;
