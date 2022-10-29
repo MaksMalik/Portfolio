ccc324f52ef6f2d4f3ba3481dd201296974d2fc3
Maksymilian Malik <maksymilian.malik@gmail.com>
2022-10-29 14:59:25 +0200

    testing background, style

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,6 +3,21 @@ import "./NavBar.css"
 
 export default function NavBar() {
   return (
-    <div className='NavBar'>NavBar</div>
+    <div className='NavBar'>
+      <ul>
+        <li>
+          <a href='#d' data-text="&nbsp;HOME">&nbsp;HOME&nbsp;</a>
+        </li>
+        <li>
+          <a href='#d' data-text="&nbsp;SKILLS">&nbsp;SKILLS&nbsp;&nbsp;</a>
+        </li>
+        <li>
+          <a href='#d' data-text="&nbsp;PORTFOLIO">&nbsp;PORTFOLIO&nbsp;&nbsp;&nbsp;&nbsp;</a>
+        </li>
+        <li>
+          <a href='#d' data-text="&nbsp;CONTACT">&nbsp;CONTACT&nbsp;&nbsp;&nbsp;</a>
+        </li>
+      </ul>
+    </div>
   )
 }
