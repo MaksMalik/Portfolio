f1fbce1354256a11e96943a917c5e8ab03ddb7c9
Maksymilian Malik <maksymilian.malik@gmail.com>
2022-10-29 14:34:48 +0200

    testing background, style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import "./reset.css"
 import MainPage from './Components/MainPage/MainPage';
 import NavBar from './Components/NavBar/NavBar';
+import Skills from './Components/Skills/Skills';
 
 function App() {
   return (
     <>
       <NavBar />
-      <MainPage/>
+      <MainPage />
+      <Skills/>
     </>
 
   );
