fab7cddbaf66d334f5481036dcaf98549b2ecdd2
Maksymilian Malik <maksymilian.malik@gmail.com>
2022-10-31 23:04:05 +0100

    Add github and linkedin links to navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -51,50 +51,45 @@ export default function NavBar() {
           <a className="link" href='#CONTACT' data-text="&nbsp;CONTACT">&nbsp;CONTACT&nbsp;&nbsp;&nbsp;</a>
         </li>
       </ul>
-    </div>
-
-    <div className='NavBar PC'>
-      <ul>
-        <li>
-          <a className="link active" href='#HOME' data-text="&nbsp;HOME">&nbsp;HOME&nbsp;</a>
-        </li>
-        <li>
-          <a className="link" href='#SKILLS' data-text="&nbsp;SKILLS">&nbsp;SKILLS&nbsp;&nbsp;</a>
-        </li>
-        <li>
-          <a className="link" href='#PORTFOLIO' data-text="&nbsp;PORTFOLIO">&nbsp;PORTFOLIO&nbsp;&nbsp;&nbsp;&nbsp;</a>
-        </li>
-        <li>
-          <a className="link" href='#CONTACT' data-text="&nbsp;CONTACT">&nbsp;CONTACT&nbsp;&nbsp;&nbsp;</a>
-        </li>
-      </ul>
-    </div>
-
-
-    <nav className="navbarM">
-      <div className="navbar-container container">
-          <input type="checkbox" name="" id=""></input>
-          <div className="hamburger-lines">
-              <span className="line line1"></span>
-              <span className="line line2"></span>
-              <span className="line line3"></span>
-          </div>
-          <ul className="menu-items">
-            <li>
-              <a className="link active" href='#HOME' data-text="&nbsp;HOME">&nbsp;HOME&nbsp;</a>
-            </li>
-            <li>
-              <a className="link" href='#SKILLS' data-text="&nbsp;SKILLS">&nbsp;SKILLS&nbsp;&nbsp;</a>
-            </li>
-            <li>
-              <a className="link" href='#PORTFOLIO' data-text="&nbsp;PORTFOLIO">&nbsp;PORTFOLIO&nbsp;&nbsp;&nbsp;&nbsp;</a>
-            </li>
-            <li>
-              <a className="link" href='#CONTACT' data-text="&nbsp;CONTACT">&nbsp;CONTACT&nbsp;&nbsp;&nbsp;</a>
-            </li>
-          </ul>
+      <div className='Navbar__icons'>
+          <a href='https://www.linkedin.com/in/maksmalik' target="_blank" rel="noreferrer"><div className='Navbar__icon icon1'></div></a>
+          <a href='https://github.com/MaksMalik' target="_blank" rel="noreferrer"><div className='Navbar__icon icon2'></div></a>
       </div>
-  </nav>
+    </div>
+
+
+
+
+      <nav className="navbarM">
+        <div className="navbar-container container">
+            <input type="checkbox" name="" id=""></input>
+            <div className="hamburger-lines">
+                <span className="line line1"></span>
+                <span className="line line2"></span>
+                <span className="line line3"></span>
+            </div>
+            <ul className="menu-items">
+              <li>
+                <a className="link active" href='#HOME' data-text="&nbsp;HOME">&nbsp;HOME&nbsp;</a>
+              </li>
+              <li>
+                <a className="link" href='#SKILLS' data-text="&nbsp;SKILLS">&nbsp;SKILLS&nbsp;&nbsp;</a>
+              </li>
+              <li>
+                <a className="link" href='#PORTFOLIO' data-text="&nbsp;PORTFOLIO">&nbsp;PORTFOLIO&nbsp;&nbsp;&nbsp;&nbsp;</a>
+              </li>
+              <li>
+                <a className="link" href='#CONTACT' data-text="&nbsp;CONTACT">&nbsp;CONTACT&nbsp;&nbsp;&nbsp;</a>
+              </li>
+              <li>
+                <div className='Navbar__icons'>
+                  <a href='https://www.linkedin.com/in/maksmalik' target="_blank" rel="noreferrer"><div className='Navbar__icon icon1'></div></a>
+                  <a href='https://github.com/MaksMalik' target="_blank" rel="noreferrer"><div className='Navbar__icon icon2'></div></a>
+                </div>
+              </li>
+            </ul>
+        </div>
+      </nav>
     </>
   )
 }
